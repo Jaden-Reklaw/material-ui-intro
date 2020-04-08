23266eecc89f6906ed82dc47d730ce05c12bae3e
Jordan Walker <jwalker117@gmail.com>
2020-04-08 14:01:52 -0500

    Edited App.js to complete lecture

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button'; //Bring in material UI
+import DeleteIcon from '@material-ui/icons/DeleteForever';
+import ArrowsIcon from '@material-ui/icons/CompareArrows';
+
+
+import './App.css';
 
 const styles = theme => ({
   button: {
@@ -11,30 +14,40 @@ const styles = theme => ({
 });
 
 class App extends Component {
-  handleClick = (event) => {
-    console.log('handleClick');
+
+  handleClick = () => {
+    alert('Thanks!')
   }
-  
+
   render() {
     const classes = this.props.classes;
     return (
-      <div className="App">
+      <>
         <header>
           <h1>Material UI Intro</h1>
         </header>
         <main>
-          <p>Hey! Make sure to use the version 3 docs! (They don't use Hooks!)</p>
-          <a href="https://v3.material-ui.com/">Material UI Docs</a>
+          <p>
+            Hey! Make sure to use the version 3 docs! (They don't use hooks!)
+            <a href="https://v3.material-ui.com/">https://v3.material-ui.com/</a>
+          </p>
+          <p>
+            Install with `npm install @material-ui/core`. This will get the 
+            latest version of Material UI. (Watch out for potential differences, but 
+            so far I haven't had any trouble with this.)
+          </p>
+          <section>
+            <h2>Demos</h2>
+            <Button variant="contained" color="secondary" 
+                    onClick={this.handleClick} className={classes.button}>
+              <DeleteIcon />Click Me!
+            </Button>
+            <Button variant="contained" className={classes.button}>
+              <ArrowsIcon />Cancel
+            </Button>
+          </section>
         </main>
-        <section>
-        <Button 
-          variant="contained" 
-          color="secondary" 
-          onClick={this.handleClick}
-          className={classes.button}
-        >Secondary</Button>
-        </section>
-      </div>
+      </>
     );
   }
 }
